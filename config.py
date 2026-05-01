47e09af44e1fe5efaab529d0e318b2c119543a28
aguiro16 <aguiro16@gmail.com>
2026-05-01 22:09:42 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+BINANCE_BASE_URL         = "https://api.binance.com"
+BINANCE_FUTURES_BASE_URL = "https://fapi.binance.com"
+
+BINANCE_API_KEY    = os.environ.get("BINANCE_API_KEY", "")
+BINANCE_API_SECRET = os.environ.get("BINANCE_API_SECRET", "")
+
+TELEGRAM_TOKEN    = os.environ.get("TELEGRAM_TOKEN", "")
+TELEGRAM_CHAT_ID  = os.environ.get("TELEGRAM_CHAT_ID", "")
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
+
+ENABLE_FUTURES_TRADING = True
+ENABLE_SPOT_TRADING    = True
+
+MAX_LEVERAGE = int(os.environ.get("MAX_LEVERAGE", "5"))
